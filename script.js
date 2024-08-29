6a6dac4aeb428e2eec2fb1a8178e818a858e4823
odi <101469962+0xip@users.noreply.github.com.>
2024-08-29 10:12:50 +0300

    Add US phone number validation feedback

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const result = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
+const phoneCheck = str =>{
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+  }
+
 checkBtn.addEventListener("click", ()=>{
   if(!input.value){
       alert("Please provide a phone number");
+}else{
+    if(phoneCheck(input.value)){
+        result.textContent = `Valid US number:  ${input.value}`
+    }else{
+        result.textContent = `Invalid US number:  ${input.value}`
+    }
 }
 })
 
-const phoneCheck = str =>{
-  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-  return regex.test(str);
-}
+clearBtn.addEventListener("click", ()=>{
+    result.textContent = "";
+  })
